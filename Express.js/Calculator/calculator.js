25d95f245fe72baa8cbde9ddc3dee7ceb178c8b6
Yiwen Dai <yiwendai224@gmail.com>
2020-08-13 17:34:06 -0400

    Finish adding BMI calculator

--- Express.js/Calculator/calculator.js
+++ Express.js/Calculator/calculator.js
@@ -8,6 +8,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmiCalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/", function (req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
@@ -44,6 +48,13 @@ app.post("/", function (req, res) {
   outputString += " = " + output;
 
   res.send(outputString);
+});
+
+app.post("/bmiCalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height) / 100;
+  var output = weight / (height * height);
+  res.send("Your BMI is: " + output);
 });
 
 app.listen(3000, function () {
