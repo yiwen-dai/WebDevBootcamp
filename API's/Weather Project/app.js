7a7314eb192c326df21fed3a3135bfb094f3bb30
Yiwen Dai <yiwendai224@gmail.com>
2020-08-27 14:52:47 -0400

    Finished weather api

--- API's/Weather Project/app.js
+++ API's/Weather Project/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const location = req.body.cityName;
+  const apikey = "63029fc102e5fc4ff5a52eb424701f19";
+  const unit = req.body.unit;
+
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Toronto,ca&appid=63029fc102e5fc4ff5a52eb424701f19&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location +
+    "&appid=" +
+    apikey +
+    "&units=" +
+    unit;
 
   https.get(url, function (response) {
     console.log(response.statusCode);
@@ -18,14 +35,16 @@ app.get("/", function (req, res) {
       const image = weatherData.weather[0].icon;
       const imageURL = "http://openweathermap.org/img/wn/" + image + "@2x.png";
 
-      res.write("<h1>The weather in Toronto is currently " + desc + ".</h1>");
       res.write(
-        "<h3>The temperature is currently " + temp + " degrees Celcius.</h3>"
+        "<h1>The weather in " + location + " is currently " + desc + ".</h1>"
       );
+      res.write("<h3>The temperature is currently " + temp + " degrees.</h3>");
       res.write("<img src=" + imageURL + " alt='Weather Icon'>");
       res.send();
     });
   });
 });
 
-app.listen(3000, function () {});
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
